7a949cb3a6d75e1d8347eb8d5f460c3dcad5611d
Saveliy <savvakokh@mail.ru>
2023-07-14 12:38:54 +0300

    FINAL

--- swiper-slider.js
+++ swiper-slider.js
@@ -1,9 +1,9 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: "auto",
-  spaceBetween: 30,
-  grabCursor: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+   slidesPerView: "auto",
+   spaceBetween: 30,
+   grabCursor: true,
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
 });
